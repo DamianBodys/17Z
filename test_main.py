c27b0994d25ac108a8ee697b0e5e30f30e8de401
dbodys94 <dbodys.94@gmail.com>
2017-12-21 19:36:26 +0100

    test

---
+++ test_main.py
@@ -0,0 +1,24 @@
+"""It's very important to install in virtualenv
+pip install WebTest
+"""
+import unittest
+import webtest
+import main
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.testapp = webtest.TestApp(main.app)
+
+    def tearDown(self):
+        pass
+
+    def test_algorithms_GET_Empty(self):
+        response = self.testapp.get('/algorithms/')
+        self.assertEqual(200, response.status_int)
+        self.assertEqual('[]', response.normal_body)
+        self.assertEqual('application/json', response.content_type)
+
+
+if __name__ == '__main__':
+    unittest.main()
