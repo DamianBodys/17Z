ead6559a77ac4c84d42bfb201b96f78932cd9307
dbodys94 <dbodys.94@gmail.com>
2017-12-25 10:06:03 +0100

    tests cd

--- test_dao.py
+++ test_dao.py
@@ -12,6 +12,16 @@ class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
         self.assertEqual(0, ret_code, msg='Wrong status Code')
         self.assertCountEqual(right_algorithm_list, test_algorithm_list)
 
+    def test_AlgorithmDAO_searchindex_EmptyWithTags(self):
+        """ checks if from empty Search database nothing is returned and statuscode =0"""
+        tags = 'algo,rithm'
+        right_algorithm_list = []
+        test_algorithm_list = []
+        ret_code = dao.AlgorithmDAO.searchindex(test_algorithm_list,tags=tags)
+        self.assertNotEqual(2, ret_code, msg='Can not connect to search GAE standard - check ' + dao._ALGORITHM_SEARCH_URL)
+        self.assertEqual(0, ret_code, msg='Wrong status Code')
+        self.assertCountEqual(right_algorithm_list, test_algorithm_list)
+
 
 if __name__ == '__main__':
     unittest.main()
