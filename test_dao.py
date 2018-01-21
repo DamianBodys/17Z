49f13f03005c0568f06040685b9e87695c47d201
dbodys94 <dbodys.94@gmail.com>
2018-01-21 22:35:40 +0100

    tests cd changed docstring to Google style and edded exceptions

--- test_dao.py
+++ test_dao.py
@@ -5,7 +5,7 @@ from main import get_flexible_url
 from google.cloud import datastore
 
 
-class WrongStatusCodeError(Exception):
+class SearchWrongStatusCodeError(Exception):
     """
     Exception raised when Http status code is greater then 399 -> Http Errors
 
@@ -14,7 +14,7 @@ class WrongStatusCodeError(Exception):
         message (str): message
 
     """
-    def __init__(self, returned_http_status_code, message='Http status code from Search GAE > 399 - check ' + dao.get_search_url()):
+    def __init__(self, returned_http_status_code, message=None):
         """
         Exception constructor
 
@@ -23,13 +23,18 @@ class WrongStatusCodeError(Exception):
             message (str):  message
         """
         self.returned_http_status_code = returned_http_status_code
-        self.message = message
+        self.message = 'Http status code from Search GAE > 399 - check ' + dao.get_search_url()
 
 
-class SearchConnectionError(requests.RequestException):
+class SearchConnectionError(Exception):
     """
     Exception rised by requests to Search Database in GAE standard
     """
+
+
+class DatastoreProjectNameValueError(ValueError):
+    """Exceprion rised when there is no project value when datastore query.fetch is run"""
+    pass
 
 
 def create_test_search_algorithm_dict(id_number):
@@ -73,11 +78,11 @@ def save_test_list_to_search_app(data_list):
     Saves test algorithms to search GAE application
 
     Args:
-        data_list (list): the list of algorithmss to be written to search GAE application
+        data_list (list): the list of algorithms to be written to search GAE application
 
     Raises:
         SearchConnectionError: requests.ConnectionError
-        WrongStatusCodeError: errors from Search GAE standard
+        SearchWrongStatusCodeError: errors from Search GAE standard
 
     """
     url = dao.get_search_url()
@@ -88,31 +93,38 @@ def save_test_list_to_search_app(data_list):
                       "linkURL": item["linkURL"]}
         try:
             response = requests.post(url, json=index_data, headers={'Content-Type': 'application/json; charset=utf-8'})
-        except SearchConnectionError:
-            raise
+        except Exception:
+            raise SearchConnectionError('Problem with connection to ' + dao.get_search_url()) from None
         if response.status_code > 399:
-            raise WrongStatusCodeError(response.status_code)
+            raise SearchWrongStatusCodeError(response.status_code)
 
 
 def del_all_from_datastore():
     """
-    Delete everything from datastore
+    Delete everything from Datastore
 
-    Retrieves all kinds from datastore and for each kind deletes all entities
+    Retrieves all kinds from Datastore and for each kind deletes all entities
+
+    Raises:
+        DatastoreProjectNameValueError: no project name
+
     """
     ds = datastore.Client()
     q = ds.query(kind='__kind__')
     q.keys_only()
     kinds = []
-    for entity in q.fetch():
-        kinds.append(entity.key.id_or_name)
-    for k in kinds:
-        qk = ds.query(kind=k)
-        qk.keys_only()
-        kys = []
-        for ent in qk.fetch():
-            kys.append(ent.key)
-        ds.delete_multi(kys)
+    try:
+        for entity in q.fetch():
+            kinds.append(entity.key.id_or_name)
+        for k in kinds:
+            qk = ds.query(kind=k)
+            qk.keys_only()
+            kys = []
+            for ent in qk.fetch():
+                kys.append(ent.key)
+            ds.delete_multi(kys)
+    except ValueError:
+        raise DatastoreProjectNameValueError('Datastore query error') from None
 
 
 def del_all_from_search():
@@ -121,45 +133,51 @@ def del_all_from_search():
 
     Sends http DELETE to GAE search application
 
-    :returns: string 'Everything deleted OK' if EOK or other if Search in GAE Standard didn't behave as expected
-    :rtype: str
+    Raises:
+        SearchConnectionError: requests.RequestError
+        SearchWrongStatusCodeError: http errors from Search GAE standard
+
     """
     url = dao.get_search_url()
     try:
         response = requests.delete(url)
-    except requests.ConnectionError:
-        return 'Can not connect to GAE Standard Search'
-    if response.status_code != 200:
-        return 'GAE Standard Error - did not return 200'
-    return 'Everything deleted OK'
+    except Exception:
+            raise SearchConnectionError('Problem with connection to ' + dao.get_search_url()) from None
+    if response.status_code > 399:
+        raise SearchWrongStatusCodeError(response.status_code)
 
 
 def del_all():
     """
     Clears all databases for testing purposes to start from scratch.
 
-    :returns: string 'Everything deleted OK' if EOK or other if Search in GAE Standard didn't behave as expected
-    :rtype: str
+    Raises:
+        SearchConnectionError: requests.ConnectionError
+        SearchWrongStatusCodeError: errors from Search GAE standard
+        DatastoreProjectNameValueError: no project name for Datastore
 
-    :Example:
-
-    >>>import test_dao
-    >>>test_dao.del_all()
-    Everything deleted OK
+    Example:
+        >>>import test_dao
+        >>>test_dao.del_all()
 
     """
-
     del_all_from_datastore()
-    return del_all_from_search()
+    del_all_from_search()
 
 
 class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
     """Tests of all static methods of class AlgorithmDAO"""
     def setUp(self):
         """Deletes everything from all databases"""
-        if del_all() != 'Everything deleted OK':
+        try:
+            del_all()
+        except SearchConnectionError:
             self.fail("there was a problem while deleting everything check if search GAE standard app is OK?" +
                       dao.get_search_url())
+        except SearchWrongStatusCodeError:
+            self.fail("there was a problem while deleting everything check if value exists in search GAE standard app")
+        except DatastoreProjectNameValueError:
+            self.fail("there was error while deleting from datastore - no project name")
 
     def test_AlgorithmDAO_searchindex_Empty(self):
         """ checks if from empty Search database nothing is returned and statuscode =0"""
@@ -190,9 +208,9 @@ class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
                       "linkURL": right_algorithm_list[0]["linkURL"]}
         try:
             response = requests.post(url, json=index_data, headers={'Content-Type': 'application/json; charset=utf-8'})
-        except SearchConnectionError:
+        except Exception:
             self.fail(msg='Can not connect to search GAE standard while adding test data - check ' + dao.get_search_url())
-        if response.status_code != 200:
+        if response.status_code > 399:
             self.fail(msg='Wrong status code while adding test data to search GAE standard ' + dao.get_search_url())
         test_algorithm_list = []
         ret_code = dao.AlgorithmDAO.searchindex(test_algorithm_list)
@@ -202,13 +220,13 @@ class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
 
     def test_AlgorithmDAO_searchindex_300Algorithms(self):
         """ checks if from 300 algorithms Search database exactly 300 algorithms is returned and statuscode =0"""
-        right_algorithm_list = []
-        create_test_search_algorithm_list(300)
-        result = save_test_list_to_search_app(right_algorithm_list)
-        if result == 2:
-            self.fail(msg='Can not connect to search GAE standard while adding test data - check ' + dao.get_search_url())
-        elif result == 1:
-            self.fail(msg='Wrong status code while adding test data to search GAE standard ' + dao.get_search_url())
+        right_algorithm_list = create_test_search_algorithm_list(300)
+        try:
+            save_test_list_to_search_app(right_algorithm_list)
+        except SearchWrongStatusCodeError as err:
+            self.fail(err.message)
+        except SearchConnectionError as err:
+            self.fail(err)
         test_algorithm_list = []
         ret_code = dao.AlgorithmDAO.searchindex(test_algorithm_list)
         self.assertNotEqual(2, ret_code, msg='Can not connect to search GAE standard - check ' + dao.get_search_url())
@@ -220,13 +238,14 @@ class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
         for tag 'algorithmSummary69'"""
         searched_string = 'algorithmSummary69'
         right_algorithm_list = create_test_search_algorithm_list(300)
-        result = save_test_list_to_search_app(right_algorithm_list)
         expected_list=[]
         expected_list.append(right_algorithm_list[69])
-        if result == 2:
-            self.fail(msg='Can not connect to search GAE standard while adding test data - check ' + dao.get_search_url())
-        elif result == 1:
-            self.fail(msg='Wrong status code while adding test data to search GAE standard ' + dao.get_search_url())
+        try:
+            save_test_list_to_search_app(right_algorithm_list)
+        except SearchWrongStatusCodeError as err:
+            self.fail(err.message)
+        except SearchConnectionError as err:
+            self.fail(err)
         test_algorithm_list = []
         ret_code = dao.AlgorithmDAO.searchindex(test_algorithm_list, tags=searched_string)
         self.assertNotEqual(2, ret_code, msg='Can not connect to search GAE standard - check ' + dao.get_search_url())
@@ -245,9 +264,13 @@ class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
         checks if get of nonexistent algorithmID Search database returns other status code then 200
         so getindex returns 1
         """
-        list_of_algorithms_to_put = []
-        create_test_search_algorithm_list(5)
-        save_test_list_to_search_app(list_of_algorithms_to_put)
+        list_of_algorithms_to_put = create_test_search_algorithm_list(5)
+        try:
+            save_test_list_to_search_app(list_of_algorithms_to_put)
+        except SearchWrongStatusCodeError as err:
+            self.fail(err.message)
+        except SearchConnectionError as err:
+            self.fail(err)
         algorithm_id_to_get = 'WrongAlgorithmID'
         returned = dao.AlgorithmDAO.getindex(algorithm_id_to_get)
         self.assertNotEqual(2, returned, msg='Can not connect to search GAE standard - check ' + dao.get_search_url())
@@ -260,10 +283,10 @@ class DaoUnittestAlgorithmDaoTestCase(unittest.TestCase):
         list_of_algorithms_to_put = create_test_search_algorithm_list(5)
         try:
             save_test_list_to_search_app(list_of_algorithms_to_put)
-        except WrongStatusCodeError as err:
+        except SearchWrongStatusCodeError as err:
             self.fail(err.message)
-        except SearchConnectionError:
-            self.fail('Can not connect to search GAE standard - check ' + dao.get_search_url())
+        except SearchConnectionError as err:
+            self.fail(err)
         algorithm_id_to_get = 'algorithmId2'
         right_dict = create_test_search_algorithm_dict(2)
         returned = dao.AlgorithmDAO.getindex(algorithm_id_to_get)
